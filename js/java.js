0e77d26a9ead9b77f3e43c2576abac19a2d11a1e
jhswang <juliehs.wang@gmail.com>
2015-08-13 18:45:16 +0000

    our project

---
+++ js/java.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+$( function(){
+	$( ".card1" ).on("click", function(){
+		$( this ).toggleClass( "flipped" );
+	})
+ })
+  
+$( function(){
+	$( ".card2" ).on("click", function(){
+		$( this ).toggleClass( "flipped" );
+	})
+ })
+
+$( function(){
+	$( ".card3" ).on("click", function(){
+		$( this ).toggleClass( "flipped" );
+	})
+ })
+
+});
---
+++ js/sudan.js
@@ -0,0 +1,64 @@
+$(document).ready(function(){
+  
+//imageHover
+  
+//slideUp
+	$("#b1").click(function(){
+		$(".box-content1").slideUp();
+	});
+		 
+	//slideDown
+	$("#b2").click(function(){
+		$(".box-content1").slideDown();
+	});
+
+	 //slideToggle
+	 $(".box-header1").click(function(){
+		$(".box-content1").slideToggle();
+	});
+  
+ //slideUp
+	$("#b3").click(function(){
+		$(".box-content2").slideUp();
+	});
+		 
+	//slideDown
+	$("#b4").click(function(){
+		$(".box-content2").slideDown();
+	});
+
+	 //slideToggle
+	 $(".box-header2").click(function(){
+		$(".box-content2").slideToggle();
+	});
+   
+   //slideUp
+	$("#b5").click(function(){
+		$(".box-content3").slideUp();
+	});
+		 
+	//slideDown
+	$("#b6").click(function(){
+		$(".box-content3").slideDown();
+	});
+
+	 //slideToggle
+	 $(".box-header3").click(function(){
+		$(".box-content3").slideToggle();
+	});
+  
+   //slideUp
+	$("#b7").click(function(){
+		$(".box-content4").slideUp();
+	});
+		 
+	//slideDown
+	$("#b8").click(function(){
+		$(".box-content4").slideDown();
+	});
+
+	 //slideToggle
+	 $(".box-header4").click(function(){
+		$(".box-content4").slideToggle();
+	});
+})
